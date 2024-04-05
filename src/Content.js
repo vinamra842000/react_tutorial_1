8f82ab5d34c1bb5fc6c1c5c84afdd4d8d782d4a7
VinamraBhavsar <vinamra842000@gmail.com>
2024-04-05 17:33:36 -0400

    useState Hook

--- src/Content.js
+++ src/Content.js
@@ -1,27 +1,38 @@
-import React from 'react'
+// useState Hook
+import { useState } from "react";
 
 const Content = () => {
+    // Like getter Setter
+    const[name, setName] = useState('Vinamra'); // Default Value Vinamra Pass
+    const[count, setCount] = useState(0);
+
     const handleNameChange = () => {
         const names = ['Vinamra', 'Bob', 'Kevin'];
         const num = Math.floor(Math.random() * 3);
-        return names[num];
+        // return names[num];
+        setName(names[num]) // Set Name as current state
     }
     const handleClick = () => {
-        console.log("You clicked.")
+        setCount(count + 1);
+        setCount(count + 1);
+        console.log(count)
     }
-    const handleClick2 = (name) => {
-        console.log(`${name} clicked!`)
-    }
+    // const handleClick2 = (name) => {
+    //     console.log(`${name} clicked!`)
+    // }
     const handleClick3 = (e) => {
         console.log(e.target)
     }
     return (
         <main>
             <p>
-                Hello {handleNameChange()}!
+                Hello {name}! {/*current State value*/}
             </p>
-            <button onClick={handleClick}>Click It!</button>
-            <button onClick={() => {handleClick2('Vinamra')}}>Click It!</button>
+            <p>
+                {count}
+            </p>
+            <button onClick={handleNameChange}>Change Name</button>
+            <button onClick={handleClick}>Change Count</button>
             <button onClick={(e) => {handleClick3(e)}}>Click It!</button>
         </main>
     )
