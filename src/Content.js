e1b4e910a9512e6e638d373ab5d4067c082b66d6
VinamraBhavsar <vinamra842000@gmail.com>
2024-04-05 17:18:36 -0400

    Simple OnClick Function Vanilla JS style

--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,23 @@ const Content = () => {
         const num = Math.floor(Math.random() * 3);
         return names[num];
     }
+    const handleClick = () => {
+        console.log("You clicked.")
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} clicked!`)
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target)
+    }
     return (
         <main>
             <p>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click It!</button>
+            <button onClick={() => {handleClick2('Vinamra')}}>Click It!</button>
+            <button onClick={(e) => {handleClick3(e)}}>Click It!</button>
         </main>
     )
 }
