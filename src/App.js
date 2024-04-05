d732d39d23335cc3a678dc7fdc7f76b56f5a0b08
VinamraBhavsar <vinamra842000@gmail.com>
2024-04-05 16:45:02 -0400

    Normal Function Call in App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['Vinamra', 'Bob', 'Kevin'];
+    const num = Math.floor(Math.random() * 3);
+    return names[num];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload what i chnage.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Hello {handleNameChange()}!
+        </p>
       </header>
     </div>
   );
